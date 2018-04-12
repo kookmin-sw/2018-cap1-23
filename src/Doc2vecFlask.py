1a2ea397d3715ba7e70a6d3b10444f72fd9531ca
lsebb1007 <js03110@naver.com>
2018-04-12 21:46:03 +0900

    결과 한 화면에서 보여주기

--- src/Doc2vecFlask.py
+++ src/Doc2vecFlask.py
@@ -3,7 +3,6 @@ from flask_restful import Resource,Api
 from flask import Flask, redirect, url_for, request, render_template
 import json
 from flask_jsonpify import jsonify
-#from flask_oauthlib.provider import OAuth2Provider
 import gensim
 import sys
 import imp
@@ -14,11 +13,15 @@ app = Flask(__name__)
 @app.route('/success/<text>')
 def success_doc2vec(text):
        model = gensim.models.Doc2Vec.load("/home/nlpserver/Desktop/Lee/model/doc2vec.model")
-      # print(type(text.encode('utf8')))
-       result = model.most_similar(text,topn=20)
-      # return json.dumps(result,ensure_ascii=False,encoding='utf-8') 
-      # print("=====================",result)
-       return json.dumps(result, ensure_ascii=False) 
+
+       result = model.most_similar(text.strip(),topn=10)
+       output = []
+       for i in range(len(result)):
+           output.append(result[i][0])
+       text = ""
+       for i in range(len(output)):
+           text += output[i] + ", "
+       return redirect(url_for("_html", result=text[:-2]))
 
 @app.route('/doc2vec', methods = ['POST','GET'])
 def get_text():
@@ -29,9 +32,12 @@ def get_text():
              user1 = request.args.get('get_text')
              return redirect(url_for('success_doc2vec',text = user1))
 
+
 @app.route('/')
-def index():
-       return "hello world"
+@app.route('/html')
+@app.route('/<result>')
+def _html(result = ""):
+	return render_template('Doc2vec.html',result=result)
        
 if __name__ == '__main__' :
-       app.run(host='0.0.0.0',debug=True) 
+       app.run(host='0.0.0.0',debug=True)
